6bc56348934423bbb5132de61ed4ad91bda130a7
Jakub Žoldák <jakub.zoldak@gmail.com>
2022-03-30 17:05:46 +0200

    Message

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -1,4 +1,4 @@
-export const GAME_TITLE = 'Slovenske wordle! Zmenené 2:)'
+export const GAME_TITLE = 'Slovenske wordle! Verzia 3)'
 
 export const WIN_MESSAGES = ['Great Job!', 'Awesome', 'Well done!']
 export const GAME_COPIED_MESSAGE = 'Game copied to clipboard'
